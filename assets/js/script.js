4da443ccd9176309fb038cdc85fd54d473d8f8ec
Duy Dinh <dvman71@gmail.com>
2016-10-09 08:55:54 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,4 +14,24 @@ $(document).ready(function () {
     $('#link').on('select2:selecting', function () {
         window.open($(this).val(), '_blank');
     })
+
+    $('.partner-wr').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow:'<button type="button" data-role="none" class="slick-prev slick-arrow" aria-label="Previous" role="button" style="display: block;"><i class="fa fa-angle-left" aria-hidden="true"></i></button>',
+        nextArrow:'<button type="button" data-role="none" class="slick-next slick-arrow" aria-label="Next" role="button" style="display: block;"><i class="fa fa-angle-right" aria-hidden="true"></i></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
 });
